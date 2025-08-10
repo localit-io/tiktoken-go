41a8b170c294947d8af68da944839e62f518c9b1
Timofei Kuzmin <sinnerpocht@gmail.com>
2025-08-10 04:24:22 +0400

    Feat: adds support for gpt-5 models

--- tiktoken_test.go
+++ tiktoken_test.go
@@ -1,6 +1,7 @@
 package tiktoken
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -39,4 +40,45 @@ func TestDecoding(t *testing.T) {
 	sourceTokens := []int{15339, 1917, 0, 57668, 53901, 3922, 3574, 244, 98220, 6447}
 	txt := enc.Decode(sourceTokens)
 	ass.Equal("hello world!你好，世界！", txt, "Decoding should be equal")
+}
+
+func TestEncodingForModel_Names(t *testing.T) {
+	for model := range MODEL_TO_ENCODING {
+		// we don't support gpt2 model so far
+		if model == "gpt2" {
+			continue
+		}
+		t.Run("Check model "+model, func(t *testing.T) {
+			t.Parallel()
+			testEncodingForModel(t, model)
+		})
+	}
+}
+
+func TestEncodingForModel_Prefixes(t *testing.T) {
+	for prefix := range MODEL_PREFIX_TO_ENCODING {
+		t.Run("Check prefix "+prefix, func(t *testing.T) {
+			t.Parallel()
+			testEncodingForModel(t, prefix)
+		})
+	}
+}
+
+func testEncodingForModel(t *testing.T, model string) {
+	text := "hello world"
+
+	ass := assert.New(t)
+	req := require.New(t)
+
+	t.Helper()
+	tkm, err := EncodingForModel(model)
+	req.NoErrorf(err, "error getting encoding for model %q: %v", model, err)
+	ass.NotNil(tkm, "Encoding for model %s should not be nil", model)
+
+	encText := tkm.Encode(text, nil, nil)
+	//t.Logf("Encoding for model %s: %v", model, encText)
+	ass.Len(encText, 2, "Encoding should be equal")
+
+	decText := tkm.Decode(encText)
+	ass.Equal(text, decText, "decoding mismatch - want: %s, got: %s", text, decText)
 }
